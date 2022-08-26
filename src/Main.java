16de6c92bd8fa24e6bb772100fddf3b0d65618d3
Ilya201091 <ZiK201091@ya.ru>
2022-08-27 01:44:08 +0700

    первый коммит

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,24 @@ public class Main {
 //    {System.out.println("Итерация цикла " + a);}
 
         //Задача 2
-        for (int i = 5; i <= 31; i=i+7)
+//        for (int i = 5; i <= 31; i=i+7)
 
-        {System.out.println("Сегодня пятница " + i +  " отчетный день" ); continue;}
+//        {System.out.println("Сегодня пятница " + i +  " отчетный день" ); continue;}
 
         //Задача 3
+//        int a = 2020;
+//       for ( int i = 79; i <= a ; i = i + 79) {System.out.println("Комета пролетает " + i);}
+//        int a = 1817;
+//        int b = 2122;
+//        for (a = 1817; a <= b; a = a + 79) {System.out.println("Комета была в " + a + " годах");}
+
+
+
+
+
+
+
+
 
 
 
