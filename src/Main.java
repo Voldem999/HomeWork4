1f94145458ede9cbad4fcf069b36c3806078c0d5
Ilya201091 <ZiK201091@ya.ru>
2022-08-26 22:32:55 +0700

    первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import static java.lang.System.out;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) {System.out.println("Hello World!");
+
+        // Задача 1
+
+//        int i = 1;
+//        while (i <= 10)
+//        {System.out.println(i);i++;}
+        //    int a = 10;
+//    for (a = 10; a >= 1; a--)
+//    {System.out.println("Итерация цикла " + a);}
+
+        //Задача 2
+
+
 
 
 
