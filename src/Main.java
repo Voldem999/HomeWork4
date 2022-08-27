f3660ec9ab5e590a3885c52102d932614b341d96
Ilya201091 <ZiK201091@ya.ru>
2022-08-27 17:43:22 +0700

    выполнил ДЗ

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,23 @@ public class Main {
 //        int i = 1;
 //        while (i <= 10)
 //        {System.out.println(i);i++;}
-        //    int a = 10;
-//    for (a = 10; a >= 1; a--)
-//    {System.out.println("Итерация цикла " + a);}
+//        int a = 10;
+//        for (a = 10; a >= 1; a--)
+//        {System.out.println("Итерация цикла " + a);}
 
         //Задача 2
 //        for (int i = 5; i <= 31; i=i+7)
 
-//        {System.out.println("Сегодня пятница " + i +  " отчетный день" ); continue;}
+//        {System.out.println("Сегодня пятница " + i +  "-е число, отчетный день" ); continue;}
 
         //Задача 3
-        int a = 2020;
-       for ( int i = 79; i <= a ; i = i + 79) {System.out.println("Комета пролетает " + i);}
-        int a = 1817;
-       int b = 2122;
-        for (a = 1817; a <= b; a = a + 79) {System.out.println("Комета была в " + a + " годах");}
+        int nowYear = 2022;
+        for ( int i = 79; i <= nowYear ; i = i + 79) {System.out.println("Комета пролетает " + i);}
+        int start = nowYear - 200;
+        int finish = nowYear + 100;
+        for (int yaer = start; yaer < finish; yaer++) {
+            if (yaer % 79 == 0)
+                System.out.println(yaer);}
 
 
 
